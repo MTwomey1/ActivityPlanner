d10e6156bf5245b2329461803b48f10c9cba221c
Mark Twomey <mark2twomey@hotmail.com>
2017-12-05 21:16:38 +0000

    Create plans

---
+++ app/src/main/java/com/example/mark/activityplanner/CreatePlans.java
@@ -0,0 +1,13 @@
+package com.example.mark.activityplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreatePlans extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_plans);
+    }
+}
--- app/src/main/java/com/example/mark/activityplanner/PlannerFragment.java
+++ app/src/main/java/com/example/mark/activityplanner/PlannerFragment.java
@@ -1,12 +1,14 @@
 package com.example.mark.activityplanner;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,9 +18,10 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PlannerFragment extends Fragment {
+public class PlannerFragment extends Fragment implements View.OnClickListener {
 
     private ListView lv;
+    Button btn_create_plans;
 
     public PlannerFragment() {
         // Required empty public constructor
@@ -40,8 +43,25 @@ public class PlannerFragment extends Fragment {
                 android.R.layout.simple_list_item_1,
                 mArrayList);
         lv.setAdapter(arrayAdapter);
+
+        btn_create_plans = view.findViewById(R.id.btn_create_id);
+        btn_create_plans.setOnClickListener(this);
+
         // Inflate the layout for this fragment
         return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+
+            case R.id.btn_create_id:{
+                Intent createIntent = new Intent(getActivity(), CreatePlans.class);
+                startActivity(createIntent);
+
+                break;
+            }
+
+        }
+    }
 }
