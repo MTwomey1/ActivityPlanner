e5a781bad1c88c775dad7ea5b03d5599a8df2472
Mark Twomey <mark2twomey@hotmail.com>
2018-03-20 18:02:25 +0000

    Group Chat Improved

--- app/src/main/java/com/example/mark/activityplanner/ChatRoom.java
+++ app/src/main/java/com/example/mark/activityplanner/ChatRoom.java
@@ -4,12 +4,21 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.bumptech.glide.signature.ObjectKey;
+import com.example.mark.activityplanner.model.ChatMessage;
+import com.example.mark.activityplanner.model.Status;
+import com.example.mark.activityplanner.model.UserType;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,6 +28,8 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -32,12 +43,18 @@ public class ChatRoom extends AppCompatActivity implements View.OnClickListener 
     EditText input_msg;
     TextView tv_conversation;
     private String temp_key;
+    private ArrayList<ChatMessage> chatMessages;
+    private ListView chatListView;
+    private ChatListAdapter listAdapter;
+    private EditText chatEditText1;
+    private ImageView enterChatView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_room);
+        setContentView(R.layout.activity_chat);
         SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         Bundle bundle = getIntent().getExtras();
         String planStr = bundle.getString("Plan");
@@ -48,11 +65,13 @@ public class ChatRoom extends AppCompatActivity implements View.OnClickListener 
         plan_id = myPlan.getPlan_id();
         username = sharedPref.getString("username","");
 
-        input_msg = findViewById(R.id.et_message_id);
-        btn_send = findViewById(R.id.btn_send_id);
-        tv_conversation = findViewById(R.id.tv_conversation_id);
+        chatMessages = new ArrayList<>();
 
-        btn_send.setOnClickListener(this);
+        chatListView = (ListView) findViewById(R.id.chat_list_view);
+
+        chatEditText1 = (EditText) findViewById(R.id.chat_edit_text1);
+        enterChatView1 = (ImageView) findViewById(R.id.enter_chat1);
+
 
         root = FirebaseDatabase.getInstance().getReference().child(plan_id);
         root.addChildEventListener(new ChildEventListener() {
@@ -82,9 +101,19 @@ public class ChatRoom extends AppCompatActivity implements View.OnClickListener 
 
             }
         });
+
+        listAdapter = new ChatListAdapter(chatMessages, this);
+
+        chatListView.setAdapter(listAdapter);
+
+        chatEditText1.setOnKeyListener(keyListener);
+
+        enterChatView1.setOnClickListener(clickListener);
+
+        chatEditText1.addTextChangedListener(watcher1);
     }
 
-    private String chat_msg, chat_username;
+    private String chat_msg, chat_username, tStamp;
 
     private void append_chat_conversation(DataSnapshot dataSnapshot) {
         Iterator i = dataSnapshot.getChildren().iterator();
@@ -92,31 +121,124 @@ public class ChatRoom extends AppCompatActivity implements View.OnClickListener 
         while(i.hasNext()){
 
             chat_msg = (String) ((DataSnapshot)i.next()).getValue();
+            tStamp = (String) ((DataSnapshot)i.next()).getValue();
             chat_username = (String) ((DataSnapshot)i.next()).getValue();
 
-            tv_conversation.append(chat_username + " : " + chat_msg + " \n\n");
+            final ChatMessage message = new ChatMessage();
+            message.setMessageStatus(Status.SENT);
+            message.setMessageText(chat_msg);
+
+            if(chat_username.equals(username)){
+                message.setUserType(UserType.OTHER);
+            }
+            else{
+                message.setUserType(UserType.SELF);
+                message.setMessageUser(chat_username);
+            }
+            long timestamp = Long.parseLong(tStamp);
+
+            message.setMessageTime(timestamp);
+            chatMessages.add(message);
         }
+        chatListView.setSelection(listAdapter.getCount()-1);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
 
-            case R.id.btn_send_id:{
+        }
+    }
+
+    private EditText.OnKeyListener keyListener = new View.OnKeyListener() {
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+            // If the event is a key-down event on the "enter" button
+            if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                // Perform action on key press
+
+                EditText editText = (EditText) v;
+
+                if(v==chatEditText1)
+                {
+                    Map<String, Object> map = new HashMap<String, Object>();
+                    temp_key = root.push().getKey();
+                    root.updateChildren(map);
+                    long tStamp = new Date().getTime();
+                    String time = Long.toString(tStamp);
+
+                    DatabaseReference message_root = root.child(temp_key);
+                    Map<String, Object> map2 = new HashMap<String, Object>();
+                    map2.put("username", username);
+                    map2.put("msg", chatEditText1.getText().toString());
+                    map2.put("timestamp", time);
+
+                    message_root.updateChildren(map2);
+                    listAdapter.notifyDataSetChanged();
+                }
+
+                chatEditText1.setText("");
+
+                return true;
+            }
+            return false;
+
+        }
+    };
+
+    private ImageView.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            if(v==enterChatView1)
+            {
                 Map<String, Object> map = new HashMap<String, Object>();
                 temp_key = root.push().getKey();
                 root.updateChildren(map);
+                long tStamp = new Date().getTime();
+                String time = Long.toString(tStamp);
 
                 DatabaseReference message_root = root.child(temp_key);
                 Map<String, Object> map2 = new HashMap<String, Object>();
                 map2.put("username", username);
-                map2.put("msg", input_msg.getText().toString());
+                map2.put("msg", chatEditText1.getText().toString());
+                map2.put("timestamp", time);
 
                 message_root.updateChildren(map2);
+                listAdapter.notifyDataSetChanged();
+            }
 
-                break;
+            chatEditText1.setText("");
+
+        }
+    };
+
+    private final TextWatcher watcher1 = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+            if (chatEditText1.getText().toString().equals("")) {
+
+            } else {
+                enterChatView1.setImageResource(R.drawable.ic_chat_send);
+
             }
         }
-    }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if(editable.length()==0){
+                enterChatView1.setImageResource(R.drawable.ic_chat_send);
+            }else{
+                enterChatView1.setImageResource(R.drawable.ic_chat_send_active);
+            }
+        }
+    };
+
 
 }
